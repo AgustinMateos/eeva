25a4b8d5813521270d0a053420dc1abfcef8ef5e
agustin mateos <agustinrotondomateos@gmail.com>
2025-02-12 18:05:30 -0300

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import ComingSoon from "@/components/ComingSoon";
+export default function Home() {
+  return (
+   <div>
+    <ComingSoon/>
+   </div>
+  );
+}
---
+++ src/components/ComingSoon.jsx
@@ -0,0 +1,13 @@
+export default function ComingSoon() {
+    return (
+        <div className="w-full h-screen">
+            <div
+                className="w-full h-screen bg-contain bg-center bg-no-repeat"
+                style={{ backgroundImage: "url('/bgHome.svg')" }}
+            >
+            </div>
+        </div>
+    );
+}
+
+
