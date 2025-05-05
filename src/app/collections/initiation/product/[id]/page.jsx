6dcf36cd752026453fbec49c3a0e292e4e2d8195
agustin mateos <agustinrotondomateos@gmail.com>
2025-05-05 01:01:16 -0300

    page

--- src/app/collections/initiation/product/[id]/page.jsx
+++ src/app/collections/initiation/product/[id]/page.jsx
@@ -1,10 +1,13 @@
 import Product from "@/components/Product";
-
+import Footer from '@/components/Footer';
 export default function Page({ params }) {
   const { id } = params;
   return (
-    <div className="min-h-[80vh] w-full bg-gradient-to-r from-[#303F48] to-[#6D7276]">
+    <div className="min-h-[100vh] w-full bg-gradient-to-r from-[#303F48] to-[#6D7276]">
       <Product id={id} />
+      <div className="h-[315px] md:h-[415px] flex md:min-w-[1315px]">
+        <Footer />
+      </div>
     </div>
   );
 }
