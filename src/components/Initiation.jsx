11ab0a54125d13cb1d55bcc302a757e6392d4f46
agustin mateos <agustinrotondomateos@gmail.com>
2025-09-30 20:03:36 -0300

    initiation

--- src/components/Initiation.jsx
+++ src/components/Initiation.jsx
@@ -181,7 +181,7 @@ const Initiation = ({ initialData }) => {
         className="group w-full h-auto relative flex flex-col"
         aria-label={`${card.title} - ${card.price !== 'N/A' ? priceFormatter.format(card.price) : 'Price not available'}`}
       >
-        <div className="h-[315px] md:h-[589px] xl:h-[549px] 2xl:h-[650px] w-[139px] md:w-[289px] relative">
+        <div className="h-[250px] md:h-[520px] xl:h-[500px] 2xl:h-[600px] w-[139px] md:w-[289px] relative">
           <Image
             src={card.image}
             alt={card.title}
@@ -200,7 +200,7 @@ const Initiation = ({ initialData }) => {
             SEE PRODUCT
           </span>
         </div>
-        <div className="text-center mt-2">
+        <div className="text-center w-[139px] md:w-[289px]">
           <h3 className="text-[#FFFFFF] text-[10px] md:text-[12px]">{card.title}</h3>
           {card.price !== 'N/A' ? (
             <div className="text-[#CCCCCC] text-[10px] md:text-[12px] mt-1 font-semibold">
