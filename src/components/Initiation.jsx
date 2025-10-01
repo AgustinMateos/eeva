ce5ac72a34da3438b0185ae72c9242bbfe6617e9
agustin mateos <agustinrotondomateos@gmail.com>
2025-09-30 21:50:11 -0300

    initiation

--- src/components/Initiation.jsx
+++ src/components/Initiation.jsx
@@ -260,7 +260,7 @@ const Initiation = ({ initialData }) => {
 
           <div className="w-full max-w-[90%] mx-auto mt-[60px]">
             {/* First product grid (first 8 products) */}
-            <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+            <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 2xl:gap-36">
               {productCards.slice(0, 8)}
             </div>
 
